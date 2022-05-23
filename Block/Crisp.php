b3a10eb39a07ba5b87b33731d1898a5dd0a783de
Alex Dong <team.magiccart@gmail.com>
2022-05-23 20:22:28 +0700

    Get info customer LoggedIn

--- Block/Crisp.php
+++ Block/Crisp.php
@@ -3,13 +3,37 @@
 /**
  * @Author: Alex Dong
  * @Date:   2020-07-09 16:45:27
- * @Last Modified by:   Alex Dong
- * @Last Modified time: 2021-04-14 13:11:05
+ * @Last Modified by:   nguyen
+ * @Last Modified time: 2022-05-23 19:07:31
  */
 
 namespace Magepow\Crispchat\Block;
 
 class Crisp extends \Magento\Framework\View\Element\Template
 {
+    /**
+     * @var \Magento\Customer\Model\Session
+     */
+
+    protected $customer;
+
+    /**
+     * Initialize dependencies.
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Magento\Customer\Model\Session $customer
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Customer\Model\Session $customer
+
+    ) {
+        $this->customer = $customer;
+        parent::__construct($context, []);
+    }
+
+    public function getCustomer()
+    {
+        return $this->customer;
+    }
 
 }
